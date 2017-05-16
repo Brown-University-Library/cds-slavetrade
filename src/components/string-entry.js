486e0f3325337b1585ed9131bdbcdfbeb6f4e54c
Cole Hansen <cole_hansen@brown.edu>
2017-05-16 13:02:03 -0400

    Refactor string entry fields Install babel, start using ko custom components, update the admin page so the upper link works, rename the header and logo image, update webpack to use babel, update the viewmodel to register the components

--- src/components/string-entry.js
+++ src/components/string-entry.js
@@ -1,15 +1,10 @@
-ko.components.register('string-entry', {
-  viewModel: function(params) {
-    this.data = params.$raw.data;
-    this.name = params.name;
-  },
-  template: '<section id="tribe">'
-            + '<div class="form-group">'
-              + '<label for="tribe">Tribe:</label>'
-              + '<select data-bind="options: $parent.tribeOptions, optionsCaption: \'Select an item...\', value: "tribe" name="tribe" class="form-control">'
-              + '</select>'
-            + '</div>'
-          + '</section>'
-});
+import string_entry_template from './string-entry.html';
 
-ko.applyBindings();
+export default {
+  viewModel: function(params) {
+    this.data = params.data;
+    this.name = params.name;
+    this.displayName = params.displayName;
+  },
+  template: string_entry_template
+}
