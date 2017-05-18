be077515330ed7d0e83d760e3f3769d33d312ef2
Cole Hansen <cole_hansen@brown.edu>
2017-05-18 11:22:11 -0400

    Fix bug for dropdowns components Pass parent object in to entry component

--- src/components/entry.js
+++ src/components/entry.js
@@ -3,6 +3,8 @@ import entry_template from './entry.html';
 export default {
   viewModel: function(params) {
     this.data = params.data;
+    this.parent = params.parent;
+    console.log(params.parent);
   },
   template: entry_template
 }
